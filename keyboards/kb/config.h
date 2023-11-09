870cab359907c9b371ad9aedaa116a05ca3d4251
nicholasung <nicholas.ung0@gmail.com>
2023-11-09 13:13:47 -0800

    restared firmware for simplified layout

--- keyboards/kb/config.h
+++ keyboards/kb/config.h
@@ -7,7 +7,7 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
+#define MANUFACTURER    nic
 #define PRODUCT         keyboard
 #define DESCRIPTION     Keyboard
 
@@ -15,44 +15,34 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
 
+#define DEBUG_MATRIX_SCAN_RATE
+#define DEBUG_ACTION
+
 /* key matrix pins */
-#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4 }
-#define MATRIX_COL_PINS { B5, B6, B7, C0, C1, C2, C3, C4, C5, C6, C7, D0, D1, D2, D3 }
+#define MATRIX_ROW_PINS { GP24, GP23, GP20, GP6, GP7 }
+#define MATRIX_COL_PINS { GP21, GP5, GP4, GP3, GP2, GP1, GP0, GP15, GP14, GP13, GP12, GP11, GP10, GP8, GP9 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
-#endif
-
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+// /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+// #define LOCKING_SUPPORT_ENABLE
 
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+// /* Locking resynchronize hack */
+// #define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
+// /* key combination for command */
+// #define IS_COMMAND() ( \
+//     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+// )
 
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
+// /* prevent stuck modifiers */
+// #define PREVENT_STUCK_MODIFIERS
 
 
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
 #endif
